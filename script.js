9106d49ad22af4d1ea72cc080b4e9bc5cdcdaac8
David <79814356+kdavidd@users.noreply.github.com>
2023-08-14 17:55:26 +0200

    Add button to show input fields only when user has clicked on add book button

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let books = document.querySelector('.books-show');
 let readCheck = document.querySelector('.read-checkbox');
 let readButton = document.querySelector('.book-read-button');
 let deleteButton = document.querySelector('.book-delete-button');
-
-
+let addBookButton= document.querySelector('.add-book-button');
+let addBooks = document.querySelector('.books-add');
 
 let myLibrary = [];
 
@@ -18,7 +18,6 @@ function Book(name, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
 }
 
 function addBookToLibrary() {
@@ -29,21 +28,20 @@ function addBookToLibrary() {
 
     if(name === "" || author === "" || isNaN(pages)) {
         alert('Please enter all details about the book');
-    } else {
+    }else {
         const book = new Book(name,author,pages,read);
         myLibrary.push(book);
-        alert('Successfully added to my library!');
         bookName.value = "";
         bookAuthor.value = "";
         bookPages.value = "";
         readCheck.checked = false;
         addBookToDisplay();
+        addBooks.style.display = "none";
     }
 
 }
 submitButton.addEventListener('click', addBookToLibrary);
 function addBookToDisplay() {
-   
     myLibrary.forEach((book) => {
         if(book.displayed === undefined) {
             let name = book.name;
@@ -75,14 +73,16 @@ function addBookToDisplay() {
             bookRead.textContent = read;
 
             if(read==="read") {
-                bookRead.style.border = "1px solid #58E07E";
+                bookRead.style.border = "2px solid #58E07E";
+                bookRead.style.backgroundColor = "#58e07e83";
+            }else {
+                bookRead.style.border = "2px solid #991D1D";
+                bookRead.style.backgroundColor = "#991d1da4";
             }
 
             let bookDelete = document.createElement('button');
             bookDelete.classList.add('book-delete-button');
             bookDelete.textContent = 'x';
-
-
 
             bookCardBottom.appendChild(bookAuthor);
             bookCardBottom.appendChild(bookRead);
@@ -95,52 +95,46 @@ function addBookToDisplay() {
     })
 }
 
-
-
-
-
-// readButton.addEventListener('click', () => {
-//     
-// });
 books.addEventListener('click', (event) => {
     const target = event.target;
     if (target.classList.contains('book-read-button')) {
         if(target.textContent === "read") {
             target.textContent = "not read";
-            target.style.backgroundColor = "#f7b55e63";
-            target.style.border = "1px solid #f7b55e";
-
-        } else {
+            target.style.border = "2px solid #991D1D";
+            target.style.backgroundColor = "#991d1da4";
+        }else {
             target.textContent = "read";
-            target.style.border = "1px solid #58E07E";
-            
-
-
+            target.style.border = "2px solid #58E07E";
+            target.style.backgroundColor = "#58e07e83";
         }
     }
 });
 
 function deleteFromLibrary(bookToDelete) {
     myLibrary = myLibrary.filter((book) => book.name !== bookToDelete);
-
     const cardToDelete = document.querySelector(`[data-cardname="${bookToDelete}"]`);
     if(cardToDelete) {
         cardToDelete.remove();
     }
 }
-// function updateReadBook(bookToDelete) {
 
-// }
+let bookCard= document.querySelector('.book-card');
+
+bookCard.addEventListener('click', ()=> {
+    deleteFromLibrary(cardName);
+}) 
 
 books.addEventListener('click', (event) => {
     const target = event.target;
     let parentElement = target.parentNode;
     let grandParentElement = parentElement.parentNode;
 
-
     if (target.classList.contains('book-delete-button')) {
        let cardName = grandParentElement.dataset.cardname;
-       console.log(cardName);
        deleteFromLibrary(cardName);
     }
 });
+
+addBookButton.addEventListener('click', ()=> {
+    addBooks.style.display = "flex";
+})
