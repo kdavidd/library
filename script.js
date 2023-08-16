422eca73fe1e90ceb43567c60d6e7d2321ac9108
David <79814356+kdavidd@users.noreply.github.com>
2023-08-16 02:57:38 +0200

    Add function to update read/not read status of book, some styling

--- script.js
+++ script.js
@@ -20,11 +20,57 @@ function Book(name, author, pages, read) {
     this.read = read;
 }
 
+function exampleBook() {
+    const book = new Book("Book example", "M.K.Edward", 255, false);
+    myLibrary.push(book);
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    bookCard.setAttribute('data-cardname', book.name);
+
+    const bookName = document.createElement('h2');
+    bookName.textContent = book.name;
+    bookName.classList.add('book-name');
+
+    const bookPages = document.createElement('h3');
+    bookPages.textContent = `${book.pages} pages`;
+    bookPages.classList.add('book-pages');
+    
+    const bookCardBottom = document.createElement('div');
+    bookCardBottom.classList.add('book-card-bottom');
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-author');
+    bookAuthor.textContent = book.author;
+
+    const readButton = document.createElement('button');
+    readButton.classList.add('book-read-button','not-read-button');
+    readButton.textContent = "not read";
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('book-delete-button');
+    deleteButton.textContent = "x";
+
+    bookCard.appendChild(bookName);
+    bookCard.appendChild(bookPages);
+
+    bookCardBottom.appendChild(bookAuthor);
+    bookCardBottom.appendChild(readButton);
+    bookCardBottom.appendChild(deleteButton);
+    bookCard.appendChild(bookCardBottom);
+
+    books.appendChild(bookCard);
+    book.displayed = true;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    exampleBook();
+});
 function addBookToLibrary() {
     let name = bookName.value;
     let author = bookAuthor.value;
     let pages = parseInt(bookPages.value);
-    let read = (readCheck.checked)? "read":"not read";
+    let read = (readCheck.checked)? true:false;
 
     if(name === "" || author === "" || isNaN(pages)) {
         alert('Please enter all details about the book');
@@ -37,9 +83,7 @@ function addBookToLibrary() {
         readCheck.checked = false;
         addBookToDisplay();
         addBooks.classList.remove("show");
-
     }
-
 }
 submitButton.addEventListener('click', addBookToLibrary);
 function addBookToDisplay() {
@@ -59,7 +103,7 @@ function addBookToDisplay() {
             bookPages.classList.add('book-pages');
 
             bookName.textContent = name;
-            bookPages.textContent = pages;
+            bookPages.textContent = `${pages} pages`;
             bookCard.appendChild(bookName);
             bookCard.appendChild(bookPages);
 
@@ -71,14 +115,13 @@ function addBookToDisplay() {
             bookAuthor.textContent = author;
             let bookRead = document.createElement('button');
             bookRead.classList.add('book-read-button');
-            bookRead.textContent = read;
 
-            if(read==="read") {
-                bookRead.style.border = "2px solid #58E07E";
-                bookRead.style.backgroundColor = "#58e07e83";
+            if(read===true) {
+                bookRead.textContent = "read";
+                bookRead.classList.add('read-button')
             }else {
-                bookRead.style.border = "2px solid #991D1D";
-                bookRead.style.backgroundColor = "#991d1da4";
+                bookRead.textContent = "not read";
+                bookRead.classList.add('not-read-button')
             }
 
             let bookDelete = document.createElement('button');
@@ -96,21 +139,6 @@ function addBookToDisplay() {
     })
 }
 
-books.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('book-read-button')) {
-        if(target.textContent === "read") {
-            target.textContent = "not read";
-            target.style.border = "2px solid #991D1D";
-            target.style.backgroundColor = "#991d1da4";
-        }else {
-            target.textContent = "read";
-            target.style.border = "2px solid #58E07E";
-            target.style.backgroundColor = "#58e07e83";
-        }
-    }
-});
-
 function deleteFromLibrary(bookToDelete) {
     myLibrary = myLibrary.filter((book) => book.name !== bookToDelete);
     const cardToDelete = document.querySelector(`[data-cardname="${bookToDelete}"]`);
@@ -118,21 +146,37 @@ function deleteFromLibrary(bookToDelete) {
         cardToDelete.remove();
     }
 }
-
-let bookCard= document.querySelector('.book-card');
-
-bookCard.addEventListener('click', ()=> {
-    deleteFromLibrary(cardName);
-}) 
-
+function updateReadButton(buttonToUpdate,isRead) {
+    if (isRead) {
+        buttonToUpdate.textContent = "read";
+        buttonToUpdate.classList.add("read-button");
+        buttonToUpdate.classList.remove("not-read-button");
+    } else {
+        buttonToUpdate.textContent = "not read";
+        buttonToUpdate.classList.add("not-read-button");
+        buttonToUpdate.classList.remove("read-button");
+    }
+}
+function updateReadBook(bookToUpdate) {
+    const cardToUpdate = document.querySelector(`[data-cardname="${bookToUpdate.name}"]`);
+    if (cardToUpdate) {
+        bookToUpdate.read = !bookToUpdate.read;
+        updateReadButton(cardToUpdate.querySelector('.book-read-button'), bookToUpdate.read);
+    }
+}
 books.addEventListener('click', (event) => {
     const target = event.target;
     let parentElement = target.parentNode;
     let grandParentElement = parentElement.parentNode;
 
-    if (target.classList.contains('book-delete-button')) {
+    if(target.classList.contains('book-delete-button')) {
        let cardName = grandParentElement.dataset.cardname;
        deleteFromLibrary(cardName);
+    }else if(target.classList.contains('book-read-button')) {
+        let cardName = grandParentElement.dataset.cardname;
+        const bookToUpdate = myLibrary.find(book => book.name === cardName);
+        
+        updateReadBook(bookToUpdate);
     }
 });
 
